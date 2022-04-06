b7e9082f36558ee70ba24072b2d3bf80afa3fb29
gsy <2576260632@qq.com>
2022-04-06 14:01:07 +0800

    gsy

---
+++ 队内赛第二队最终代码/Devices/ENCODER/encoder.c
@@ -0,0 +1,244 @@
+/**
+  ******************************************************************************
+  * @file    encoder.c
+  * @author  GSY
+  * @version V1.0
+  * @date    
+  * @brief   电机编码器
+  ******************************************************************************
+  */
+#include "encoder.h"
+/**************************************************************************
+函数功能：把TIM2初始化为编码器接口模式
+入口参数：无
+返回  值：无
+**************************************************************************/
+void Encoder_Init_TIM2(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;  
+  TIM_ICInitTypeDef TIM_ICInitStructure;  
+  GPIO_InitTypeDef GPIO_InitStructure;
+	
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);//使能定时器2的时钟
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO|RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOB, ENABLE);//使能PA PB端口时钟
+	GPIO_PinRemapConfig(GPIO_FullRemap_TIM2,ENABLE);
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable , ENABLE);
+	
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;	//端口配置
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; //浮空输入
+  GPIO_Init(GPIOA, &GPIO_InitStructure);					      //根据设定参数初始化GPIOB
+  
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;	//端口配置
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; //浮空输入
+  GPIO_Init(GPIOB, &GPIO_InitStructure);					      //根据设定参数初始化GPIOB
+	
+  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+  TIM_TimeBaseStructure.TIM_Prescaler = 0x0; // 预分频器 
+  TIM_TimeBaseStructure.TIM_Period = ENCODER_TIM_PERIOD-1; //设定计数器自动重装值
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;//选择时钟分频：不分频
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;////TIM向上计数  
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+	
+  TIM_EncoderInterfaceConfig(TIM2, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);//使用编码器模式3
+	
+  TIM_ICStructInit(&TIM_ICInitStructure); //把TIM_ICInitStruct 中的每一个参数按缺省值填入
+
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+	
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	//输入器不分频
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	//上升沿捕获
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	//映射到IC1
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+	//不滤波
+	TIM_ICInit(TIM2, &TIM_ICInitStructure);
+	
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+  TIM_ICInit(TIM2, &TIM_ICInitStructure);//根据 TIM_ICInitStruct 的参数初始化外设	TIMx
+ 
+  TIM_ClearFlag(TIM2, TIM_FLAG_Update);//清除TIM的更新标志位
+  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);//使能定时器中断
+
+  TIM_SetCounter(TIM2,0);//设置TIMx 计数器寄存器值
+  TIM_Cmd(TIM2, ENABLE); //使能定时器
+}
+/**************************************************************************
+函数功能：把TIM3初始化为编码器接口模式
+入口参数：无
+返回  值：无
+**************************************************************************/
+void Encoder_Init_TIM3(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;  
+  TIM_ICInitTypeDef TIM_ICInitStructure;  
+  GPIO_InitTypeDef GPIO_InitStructure;
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);//使能定时器2的时钟
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);//使能PA端口时钟
+	
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;	//端口配置
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; //浮空输入
+  GPIO_Init(GPIOA, &GPIO_InitStructure);					      //根据设定参数初始化GPIOB
+  
+  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+  TIM_TimeBaseStructure.TIM_Prescaler = 0x0; // 预分频器 
+  TIM_TimeBaseStructure.TIM_Period = ENCODER_TIM_PERIOD-1; //设定计数器自动重装值
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;//选择时钟分频：不分频
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;////TIM向上计数  
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+	
+  TIM_EncoderInterfaceConfig(TIM3, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);//使用编码器模式3
+	
+  TIM_ICStructInit(&TIM_ICInitStructure); //把TIM_ICInitStruct 中的每一个参数按缺省值填入
+	
+	TIM_ICInitStructure.TIM_ICFilter = 0;  //设置滤波器长度
+	TIM_ICInitStructure.TIM_Channel=TIM_Channel_1;
+	TIM_ICInitStructure.TIM_ICPolarity=TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICPrescaler=TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICSelection=TIM_ICSelection_DirectTI;
+  TIM_ICInit(TIM3, &TIM_ICInitStructure);
+	
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+  TIM_ICInit(TIM3, &TIM_ICInitStructure);//根据 TIM_ICInitStruct 的参数初始化外设	TIMx
+ 
+  TIM_ClearFlag(TIM3, TIM_FLAG_Update);//清除TIM的更新标志位
+  TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);//使能定时器中断
+
+  TIM_SetCounter(TIM3,0);//设置TIMx 计数器寄存器值
+  TIM_Cmd(TIM3, ENABLE); //使能定时器
+}
+/**************************************************************************
+函数功能：把TIM4初始化为编码器接口模式
+入口参数：无
+返回  值：无
+**************************************************************************/
+void Encoder_Init_TIM4(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;  
+  TIM_ICInitTypeDef TIM_ICInitStructure;  
+  GPIO_InitTypeDef GPIO_InitStructure;
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);//使能定时器4的时钟
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);//使能PB端口时钟
+	
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;	//端口配置
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; //浮空输入
+  GPIO_Init(GPIOB, &GPIO_InitStructure);					      //根据设定参数初始化GPIOB
+  
+  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+  TIM_TimeBaseStructure.TIM_Prescaler = 0x0; // 预分频器 
+  TIM_TimeBaseStructure.TIM_Period = ENCODER_TIM_PERIOD-1; //设定计数器自动重装值
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;//选择时钟分频：不分频
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;////TIM向上计数  
+  TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+	
+  TIM_EncoderInterfaceConfig(TIM4, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);//使用编码器模式3
+	
+  TIM_ICStructInit(&TIM_ICInitStructure); //把TIM_ICInitStruct 中的每一个参数按缺省值填入
+	//配置输入
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	//输入器不分频
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	//上升沿捕获
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	//映射到IC1
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+	//不滤波
+	TIM_ICInit(TIM4, &TIM_ICInitStructure);
+	
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+  TIM_ICInit(TIM4, &TIM_ICInitStructure);//根据 TIM_ICInitStruct 的参数初始化外设	TIMx
+ 
+  TIM_ClearFlag(TIM4, TIM_FLAG_Update);//清除TIM的更新标志位
+  TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);//使能定时器中断
+ 
+  TIM_SetCounter(TIM4,0);//设置TIMx 计数器寄存器值
+  TIM_Cmd(TIM4, ENABLE); //使能定时器
+}
+/**************************************************************************
+函数功能：把TIM5初始化为编码器接口模式
+入口参数：无
+返回  值：无
+**************************************************************************/
+void Encoder_Init_TIM5(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;  
+  TIM_ICInitTypeDef TIM_ICInitStructure;  
+  GPIO_InitTypeDef GPIO_InitStructure;
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);//使能定时器5的时钟
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);//使能PB端口时钟
+	
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1;	//端口配置
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; //浮空输入
+  GPIO_Init(GPIOA, &GPIO_InitStructure);					      
+  
+  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+  TIM_TimeBaseStructure.TIM_Prescaler = 0x0; // 预分频器 
+  TIM_TimeBaseStructure.TIM_Period = ENCODER_TIM_PERIOD-1; //设定计数器自动重装值
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;//选择时钟分频：不分频
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;////TIM向上计数  
+  TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
+	
+  TIM_EncoderInterfaceConfig(TIM5, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);//使用编码器模式3
+		//配置输入
+	
+  TIM_ICStructInit(&TIM_ICInitStructure); //把TIM_ICInitStruct 中的每一个参数按缺省值填入
+	
+	//配置输入
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	//输入器不分频
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	//上升沿捕获
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	//映射到IC1
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+	//不滤波
+	TIM_ICInit(TIM4, &TIM_ICInitStructure);
+	
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+  TIM_ICInit(TIM5, &TIM_ICInitStructure);//根据 TIM_ICInitStruct 的参数初始化外设	TIMx
+ 
+  TIM_ClearFlag(TIM5, TIM_FLAG_Update);//清除TIM的更新标志位
+  TIM_ITConfig(TIM5, TIM_IT_Update, ENABLE);//使能定时器中断
+ 
+  TIM_SetCounter(TIM5,0);//设置TIMx 计数器寄存器值
+  TIM_Cmd(TIM5, ENABLE); //使能定时器
+}
+
+
+/**************************************************************************
+函数功能：单位时间读取编码器计数
+入口参数：定时器
+返回  值：速度值
+**************************************************************************/
+int Read_Encoder(u8 TIMX)//读取计数器的值
+{
+  int Encoder_TIM;
+	switch(TIMX)
+	{
+	  case 2:Encoder_TIM=(short)TIM2->CNT; TIM2 -> CNT=0;  break;
+		case 3:Encoder_TIM=(short)TIM3->CNT; TIM3 -> CNT=0;  break;
+		case 4:Encoder_TIM=(short)TIM4->CNT; TIM4 -> CNT=0;  break;
+		case 5:Encoder_TIM=(short)TIM5->CNT; TIM5 -> CNT=0;  break;
+		default: Encoder_TIM=0;
+	}
+  return Encoder_TIM;
+}
---
+++ 队内赛第二队最终代码/Devices/follow/follow.c
@@ -0,0 +1,174 @@
+/**
+  ******************************************************************************
+  * @file    follow.c
+  * @author  GSY
+  * @version V1.0
+  * @date    
+  * @brief   任务五循迹自带放
+  ******************************************************************************
+  */
+#include "red.h"
+#include "sys.h"
+float fKp =1500, fKd = 175;                    //pid弯道参数参数 
+float foerror = 0, fP = 0, fI = 0, fD = 0, fPID_value = 0;//pid直道参数 
+float fdecide = 0;                                     //元素判断
+int fo;
+int fsensor[9] = {0, 0, 0, 0, 0};                      //5个传感器数值的数组  
+float fprevious_error = 0, fprevious_I = 0;           //误差值 
+int finitial_motor_speed = 300; 
+int fo;
+void fRED  (void) //IO初始化
+{ 
+	
+ 	GPIO_InitTypeDef GPIO_InitStructure;
+ 
+ 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG,ENABLE);//使能PORTA,PORTE时钟
+	
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9;//KEY0-KEY1
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+ 	GPIO_Init(GPIOG, &GPIO_InitStructure);//初始化GPIOE4,3
+
+}
+/**************************************************************************
+函数功能：红外PID
+入口参数：无
+返回  值：无
+**************************************************************************/
+
+void fcalc_pid()
+{
+  fP = foerror;
+  fD = foerror - fprevious_error;
+  fPID_value = (fKp * fP) + (fKd * fD);
+	
+	
+  fprevious_error = foerror;
+}
+
+void fconconcc ()
+{
+		if(foerror>300)
+			foerror=300;
+
+}
+void stopgo()
+{
+	 Target_velocity1 = 0;
+	 Target_velocity2 = 0;
+	 Target_velocity3 = 0;
+	 Target_velocity4 = 0;
+
+}
+void stopput() //撞墙编码器读取自动放
+{
+	if(Encoder1==0&&Encoder2==0)
+	{
+		stopgo();
+		TIM_SetCompare2(TIM1,500);
+		delay_ms(600);
+		TIM_SetCompare1(TIM1,800);
+		runrun =1 ;
+	}
+}
+	
+/**************************************************************************
+函数功能：红外模块检测
+入口参数：无
+返回  值：无
+**************************************************************************/
+void fread_sensor_values(void)
+{
+		fsensor[0] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_1);
+		fsensor[1] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_2);
+		fsensor[2] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_3);
+		fsensor[3] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_4);
+		fsensor[4] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_5);
+		fsensor[5] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_6);
+		fsensor[6] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_7);
+		fsensor[7] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_8);
+		fsensor[8] = GPIO_ReadInputDataBit(GPIOG,GPIO_Pin_9);
+    
+		//01  2  345  6   78
+		
+		
+	 if ((fsensor[2] == 0) && (fsensor[3] == 1) && (fsensor[4] == 1)&& (fsensor[5] == 1)&& (fsensor[6] == 0)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = 0;      //   00  0  111  0  00
+   }
+	 else	 if ((fsensor[2] == 0) && (fsensor[3] == 1) && (fsensor[4] == 1)&& (fsensor[5] == 1)&& (fsensor[6] == 1)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = 0.3;    //   00  0  111  1  00
+   }
+	 else	 if ((fsensor[2] == 1) && (fsensor[3] == 1) && (fsensor[4] == 1)&& (fsensor[5] == 1)&& (fsensor[6] == 0)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = - 0.5;    //   00  1  111  0  00
+   }
+	 else	 if ((fsensor[2] == 0) && (fsensor[3] == 0) && (fsensor[4] == 1)&& (fsensor[5] == 1)&& (fsensor[6] == 1)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = 0.5;    //   00  0  011  1  00
+   }
+	 else	 if ((fsensor[2] == 1) && (fsensor[3] == 1) && (fsensor[4] == 1)&& (fsensor[5] == 0)&& (fsensor[6] == 0)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = - 0.7;    //   00  1  110  0  00
+   }
+	 	else	 if ((fsensor[2] == 0) && (fsensor[3] == 0) && (fsensor[4] == 0)&& (fsensor[5] == 1)&& (fsensor[6] == 1)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = 0.8;    //   00  0  001  1  00
+   }
+	  	else	 if ((fsensor[2] == 1) && (fsensor[3] == 0) && (fsensor[4] == 1)&& (fsensor[5] == 0)&& (fsensor[6] == 1)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = - 0.8;    //   00  1  100  0  00
+   }
+	 	else	 if ((fsensor[2] == 1) && (fsensor[3] == 1) && (fsensor[4] == 0)&& (fsensor[5] == 0)&& (fsensor[6] == 0)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = - 0.8;    //   00  1  100  0  00
+   }
+	 	 	else	 if ((fsensor[2] == 1) && (fsensor[3] == 0) && (fsensor[4] == 0)&& (fsensor[5] == 0)&& (fsensor[6] == 0)&& (fsensor[7] == 0)&& (fsensor[8] == 0))
+   {
+      foerror = - 0.8;    //   00  1  100  0  00
+   }
+
+			else if ( (fsensor[7] == 1)&& (fsensor[8] == 1))
+	{
+			foerror = 1.2;
+	}
+				else if ((fsensor[7] == 1)&& (fsensor[8] == 0))
+	{
+			foerror = 1.2;
+	}
+		 		else if ( (fsensor[7] == 0)&& (fsensor[8] == 1))
+	{
+			foerror = 1.2 ;
+	}
+		 		else if ((fsensor[0] == 0)&& (fsensor[1] == 0)&&(fsensor[7] == 0)&& (fsensor[8] == 0))
+	{
+			foerror =  1;
+	}
+	 
+	fcalc_pid();
+	fconconcc();
+	stopput();
+  PS2_KEY=PS2_DataKey();	
+	 if(PS2_KEY == 9)
+	 {		
+			__set_FAULTMASK(1);   //STM32程序软件复位  
+			NVIC_SystemReset();  
+	}
+
+	 Target_velocity1 = -(finitial_motor_speed + fPID_value);
+	 Target_velocity2 = -(finitial_motor_speed - fPID_value);
+	 Target_velocity3 =  (finitial_motor_speed + fPID_value);
+	 Target_velocity4 = -(finitial_motor_speed - fPID_value);
+
+ }
+
+
+
+
+
+ 
+
+
+
+
